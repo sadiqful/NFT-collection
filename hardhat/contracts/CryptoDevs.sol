cbe52b86cd4ec384f04ad02b4d00cf34dc8a4377
Aliyu <sadiqful@gmail.com>
2023-02-16 20:25:41 +0100

    initial commit

---
+++ hardhat/contracts/CryptoDevs.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    string _tokenBaseURI;
+    uint256 public _price = 0.01 ether;
+    bool public _paused;
+    uint256 public maxTokenIds = 20;
+    uint256 public tokenIds;
+    IWhitelist whitelist;
+    bool public presaleStarted;
+    uint256 public presaleEnded; 
+
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    constructor (string memory baseURI, address whitelistContract) ERC721("CryptoDevs", "CD") {
+        _tokenBaseURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, 'Presale is running');
+        require(whitelist.whitelistedAddresses(msg.sender), 'You are not whitelisted');
+        require(tokenIds < maxTokenIds, 'Exceeded Maximum Crypto Devs Supply');
+        require(msg.value >= _price, 'Ether sent is not correct');
+        tokenIds += 1;
+
+        _safeMint(msg.sender, tokenIds);
+    }
+
+
+    function Mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >= presaleEnded, 'Presale has not ended yet');
+        require(tokenIds < maxTokenIds, 'Exceed Maximum Crypro Devs Supply');
+        require(msg.value >= _price, 'Ether sent is not correct');
+        tokenIds += 1;
+
+        _safeMint(msg.sender, tokenIds);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
